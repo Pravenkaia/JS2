f72ad9d6865186fe368f0f9e9c00f05ec205e67c
Pravenkaia <pravlen@rukzak.ru>
2019-04-15 23:10:02 +0300

    Сделала с имитацией ошибок

--- lesson3/js/functions.js
+++ lesson3/js/functions.js
@@ -1,43 +1,74 @@
+function randomVariant() {
+    // три числа 1  -- 60% вероятности
+    // одно число 2 -- 20 % вероятности
+    // одно число 3 -- 20 % вероятности
+    let variants = [1, 1, 1, 2, 3];
+
+    const position = randomInt(0, variants.length);
+    const variant = variants[position];
+    return variant;
+}
+
+
+function randomInt(min, max) {
+    let randNumber = min + Math.random() * (max - min);
+    randNumber = Math.floor(randNumber);
+    return randNumber;
+}
+
+
 const makeGETRequest = (url) => {
 
+    let variant = randomVariant();
 
-    let request;
 
-    //создаем новый объект кроссбраузерно
-    if (window.XMLHttpRequest) {
-        request = new XMLHttpRequest();
-    } else if (window.ActiveXObject) {
-        request = new ActiveXObject("Microsoft.XMLHTTP");
-    }
+
+
 
     return new Promise(function (resolve, reject) {
+        let request;
 
-        request.open('GET', url, true);
+        if (variant == 2) {
+            setTimeout(() => reject(new Error('Превышение времени ожидания сервера!')), 3000);
+        } else if (variant == 3) {
+            reject('Файл не найден!');
+        } else {
 
-        request.onreadystatechange = function () {
+            //создаем новый объект кроссбраузерно
+            if (window.XMLHttpRequest) {
+                request = new XMLHttpRequest();
+            } else if (window.ActiveXObject) {
+                request = new ActiveXObject("Microsoft.XMLHTTP");
+            }
 
-            if (request.readyState === 4) {
+            request.open('GET', url, true);
+            request.send();
+            request.onreadystatechange = function () {
 
-                // от 200 до 300 полезная информация, не ошибка
-                (request.status >= 200 && request.status < 300) ?
+
+                if (request.readyState === 4) {
+
+                    // от 200 до 300 полезная информация, не ошибка
+                    (request.status >= 200 && request.status < 300) ?
                     resolve(request.responseText):
-                    reject(`Ошибка! выкидываю reject(): ${request.status}`);
+                        reject(`Ошибка! выкидываю reject(): ${request.status}`);
+
+                }
+
+
+                console.log(request.readyState);
+                console.log("Ответ сервера " + request.statusText);
+                console.log("Ответ сервера " + request.responseText);
 
             }
 
-            console.log(request.readyState);
-            console.log("Ответ сервера " + request.statusText);
-            console.log("Ответ сервера " + request.responseText);
-
         }
-
-        request.send();
 
     });
 
 }
 
-const makePOSTRequest = (url,data) => {
+const makePOSTRequest = (url, data) => {
 
 
     let request;
@@ -61,7 +92,7 @@ const makePOSTRequest = (url,data) => {
 
                 // от 200 до 300 полезная информация, не ошибка
                 (request.status >= 200 && request.status < 300) ?
-                    resolve(request.statusText):
+                resolve(request.statusText):
                     reject(`Ошибка! выкидываю reject() POST: ${request.status}`);
 
             }
@@ -72,7 +103,7 @@ const makePOSTRequest = (url,data) => {
 
         }
 
-        
+
 
     });
 
